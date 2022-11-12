bcf8711ad5b18237c19b822eb57376efb43b094f
shapaj <domenik2005shapaj@gmail.com>
2022-11-12 19:34:58 +0200

    Start

---
+++ testTask/testTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Ihor on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
